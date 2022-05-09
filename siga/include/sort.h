519463f36c2afbd891af2f4e41d9924d7972720f
Jose Camata <72111360+josecamata@users.noreply.github.com>
2022-05-09 13:45:16 -0300

    Update sort.h

--- siga/include/sort.h
+++ siga/include/sort.h
@@ -2,26 +2,60 @@
 #define SORT_H__
 
 
+// Use essa função para movimentar dados 
+template <typename T>
+void swap(T* a, T *b)
+{
+    T* tmp = a;
+    a      = b;
+    b      = tmp;
+}
+
+
 template <typename T>
 void bubble_sort(T* array, int size, bool (*compare)(T&, T&))  
 {
     // TODO: Implementação do Bubble Sort
-     
-
+    // Coloque aqui a implementação da atividade passada
 }
+
+
 
 template <typename T>
 void insert_sort(T* array, int size,  bool (*compare)(T&, T&))
 {
-    // TODO: Implementação do Insert Sort 
+    // TODO: Implementação do Insert Sort
+    // Coloque aqui a implementação da atividade passada
 }
 
 
 template <typename T>
 void selection_sort(T* array, int size, bool (*compare)(T&, T&))
 {
-    // TODO: Implementação do selection Sort 
+    // TODO: Implementação do selection Sort
+    // Coloque aqui a implementação da atividade passada
 }
+
+
+
+template <typename T>
+void merge_sort(T* array, int size, bool (*compare)(T&, T&))
+{
+    // TODO: Implementação do merge_sort
+    // Coloque aqui a implementação da atividade passada
+}
+
+
+
+template <typename T>
+void quick_sort(T* array, int size, bool (*compare)(T&, T&))
+{
+    // TODO: Implementação do quicksort
+    // Coloque aqui a implementação da atividade passada
+}
+
+
+
 
 
 #endif /* SORT_H__ */
